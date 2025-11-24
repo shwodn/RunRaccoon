6adaa640bdd7dab234655750b79e56ed5244f090
NJW <shwodn123@gmail.com>
2025-11-24 22:38:45 +0900

    feat : 타일맵 시작과 끝 감지 구현

---
+++ Assets/Scripts/InGameScene/Insprctor/StartPointInspector.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class StartPointInspector : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //Debug.Log("충돌을 감지했습니다.");
+        // 타일 맵이 카메라에 들어가면 수행할 코드
+        if (collision.CompareTag("TileStartPoint")) { Debug.Log("타일 맵이 카메라에 들어오기 시작합니다."); }
+        
+    }
+}
