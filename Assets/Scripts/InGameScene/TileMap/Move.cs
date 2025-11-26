eb509d4912987fd9b5a828ed785c77c9a0eab223
NJW <shwodn123@gmail.com>
2025-11-26 10:22:34 +0900

    feat : 플레이어 물리 충돌 컴포넌트 추가, 타일멥 컴포넌트 추가 및 이동 코드 수정

--- Assets/Scripts/InGameScene/TileMap/Move.cs
+++ Assets/Scripts/InGameScene/TileMap/Move.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
-    private void Update()
+    private new Rigidbody2D rigidbody;
+    private Vector2 movePosition;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
     {
         MoveObject();
     }
 
-    private void MoveObject() { transform.position += Vector3.left * moveSpeed * Time.deltaTime; }
+    private void MoveObject() 
+    {
+        movePosition = rigidbody.position + Vector2.left * moveSpeed * Time.deltaTime;
+        rigidbody.MovePosition(movePosition); 
+    }
 }
